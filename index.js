35f6581e016f3ee9e31d8f17ce4736c9405062eb
metaldripping-admin <vishachoudhary@gmail.com>
2021-10-01 12:58:28 +0530

    !removed from messages.post and created rooms

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ import passportLocalMongoose from 'passport-local-mongoose';
 import connectEnsureLogin from 'connect-ensure-login';
 
 const app = express();
-const port = 4000;
+const port = 4567;
 
 app.use(express.static(__dirname))
 
@@ -55,15 +55,25 @@ passport.deserializeUser(User.deserializeUser())
 // User.register({ username: 'someone', active: false }, 'password')
 
 app.get('/', connectEnsureLogin.ensureLoggedIn(), (req, res) => {
-    let room = 'hall'
+    let room = req.params.room
     var api_server = 'https://api2.aleph.im'
     var network_id = 261
     var channel = 'TEST'
     aleph.posts.get_posts('chat', { 'refs': [room], 'api_server': api_server }).then((result) => {
-        res.render('index', { posts: result.posts, user: req.user })
+        res.render('index', { posts: result.posts, user: req.user, room: room })
     })
 })
-    
+
+app.get('/rooms/:room', connectEnsureLogin.ensureLoggedIn(), (req, res) => {
+    let room = req.params.room
+    var api_server = 'https://api2.aleph.im'
+    var network_id = 261
+    var channel = 'TEST'
+    aleph.posts.get_posts('chat', { 'refs': [room], 'api_server': api_server }).then((result) => {
+        res.render('index', { posts: result.posts, user: req.user, room: room })
+    })
+})
+
 app.get("/login", (req, res) => {
     res.sendFile('views/login.html', { root: __dirname })
 })
@@ -88,15 +98,17 @@ app.post("/register", (req, res) => {
 })
 
 
-
 app.post("/login", passport.authenticate('local'), (req, res) => {
     res.redirect("/")
 })
 
-app.post("/messages", connectEnsureLogin.ensureLoggedIn(), (req, res) => {
+app.post("/messages/:room", connectEnsureLogin.ensureLoggedIn(), (req, res) => {
+
     var message = req.body.message
+    const room = req.params.room
+
     aleph.ethereum.import_account({ mnemonics: req.user.mnemonics }).then((account) => {
-        let room = 'hall'
+        let room = req.params.room
         var api_server = 'https://api2.aleph.im'
         var network_id = 261
         var channel = 'TEST'
@@ -124,4 +136,3 @@ app.get('/users/:username', connectEnsureLogin.ensureLoggedIn(), (req, res) => {
 app.listen(port, () => {
     console.log(`Server running on port ${port}...`)
 })
-
